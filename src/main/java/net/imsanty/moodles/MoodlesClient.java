023646afe754bcddc77b977ba9e64fa23651e28c
ImSanty <santiaguero936@gmail.com>
2025-11-04 21:56:13 -0300

    Add initial mod files including main class, client class, data generator, mixin, and configuration

---
+++ src/main/java/net/imsanty/moodles/MoodlesClient.java
@@ -0,0 +1,12 @@
+package net.imsanty.moodles;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoodlesClient implements ClientModInitializer {
+
+  @Override
+  public void onInitializeClient() {
+    // Client-side initialization logic
+  }
+
+}
